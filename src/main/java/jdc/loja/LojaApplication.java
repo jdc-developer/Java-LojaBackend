eeed7aeb7037c60f22b4e099b50d436b893b12ef
Jorge Do Carmo <jorge.do.carmo123@gmail.com>
2018-10-24 16:36:06 -0300

    Operação de instanciação

--- src/main/java/jdc/loja/LojaApplication.java
+++ src/main/java/jdc/loja/LojaApplication.java
@@ -1,12 +1,32 @@
 package jdc.loja;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class LojaApplication {
+import jdc.loja.domain.Categoria;
+import jdc.loja.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class LojaApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository rep;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		rep.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 }
