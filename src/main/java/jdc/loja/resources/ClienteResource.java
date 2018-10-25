af50722353268d712d7771e9343a5df48ef31a59
Jorge Do Carmo <jorge.do.carmo123@gmail.com>
2018-10-25 15:14:24 -0300

    Endpoint clientes/{id}

--- src/main/java/jdc/loja/resources/ClienteResource.java
+++ src/main/java/jdc/loja/resources/ClienteResource.java
@@ -7,20 +7,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import jdc.loja.domain.Categoria;
-import jdc.loja.services.CategoriaService;
+import jdc.loja.domain.Cliente;
+import jdc.loja.services.ClienteService;
 
 @RestController
-@RequestMapping(value="/categorias")
-public class CategoriaResources {
+@RequestMapping(value="/clientes")
+public class ClienteResource {
 	
 	@Autowired
-	private CategoriaService service;
+	private ClienteService service;
 
 	@RequestMapping(value="{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria obj = service.buscar(id);
+		Cliente obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/jdc/loja/services/ClienteService.java
@@ -0,0 +1,25 @@
+package jdc.loja.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import jdc.loja.domain.Cliente;
+import jdc.loja.repositories.ClienteRepository;
+import jdc.loja.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository rep;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = rep.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()
+				));
+	}
+
+}
