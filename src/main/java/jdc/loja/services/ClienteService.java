70c838adf3eb17df173ec3aa05a072033b06222e
Jorge Do Carmo <jorge.do.carmo123@gmail.com>
2018-10-26 16:41:18 -0300

    PUT DELETE e GET para cliente

--- src/main/java/jdc/loja/services/ClienteService.java
+++ src/main/java/jdc/loja/services/ClienteService.java
@@ -1,12 +1,19 @@
 package jdc.loja.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import jdc.loja.domain.Cliente;
+import jdc.loja.dto.ClienteDTO;
 import jdc.loja.repositories.ClienteRepository;
+import jdc.loja.services.exceptions.DataIntegrityException;
 import jdc.loja.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -22,4 +29,36 @@ public class ClienteService {
 				));
 	}
 
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return rep.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			rep.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um cliente que possui pedidos");
+		}
+	}
+	
+	public List<Cliente> findAll() {
+		return rep.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageReq = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return rep.findAll(pageReq);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 }
