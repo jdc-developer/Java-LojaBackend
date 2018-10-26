9a1e072160a3aac832446d4a97a88262e6f05ba9
Jorge Do Carmo <jorge.do.carmo123@gmail.com>
2018-10-26 12:24:24 -0300

    Deletando uma categoria

--- src/main/java/jdc/loja/services/CategoriaService.java
+++ src/main/java/jdc/loja/services/CategoriaService.java
@@ -3,10 +3,12 @@ package jdc.loja.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import jdc.loja.domain.Categoria;
 import jdc.loja.repositories.CategoriaRepository;
+import jdc.loja.services.exceptions.DataIntegrityException;
 import jdc.loja.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,6 +32,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return rep.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			rep.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 
 }
