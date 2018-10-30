6a73676edb235259547cffe112df0043f110dbbc
Jorge Do Carmo <jorge.do.carmo123@gmail.com>
2018-10-30 16:17:09 -0300

    Configuração inicial do Spring Security

--- src/main/java/jdc/loja/config/TestConfig.java
+++ src/main/java/jdc/loja/config/TestConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import jdc.loja.services.DBService;
 import jdc.loja.services.EmailService;
@@ -28,4 +29,15 @@ public class TestConfig {
 	public EmailService emailService() {
 		return new MockEmailService();
 	}
+	
+	@Bean
+    public JavaMailSenderImpl mailSender() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+
+        javaMailSender.setProtocol("SMTP");
+        javaMailSender.setHost("127.0.0.1");
+        javaMailSender.setPort(25);
+
+        return javaMailSender;
+    }
 }
