28737ae8f1ff479860df6c5bff441453cff51215
Jorge Do Carmo <jorge.do.carmo123@gmail.com>
2018-10-31 17:06:00 -0300

    Enviando imagem via endpoint

--- src/main/java/jdc/loja/LojaApplication.java
+++ src/main/java/jdc/loja/LojaApplication.java
@@ -1,17 +1,11 @@
 package jdc.loja;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jdc.loja.services.S3Service;
-
 @SpringBootApplication
 public class LojaApplication implements CommandLineRunner {
-
-	@Autowired
-	private S3Service service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
@@ -19,6 +13,5 @@ public class LojaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		service.uploadFile("C:\\dbz.jpg");
 	}
 }
