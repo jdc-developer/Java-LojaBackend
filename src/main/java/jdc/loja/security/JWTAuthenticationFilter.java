9410f163d5d4d0416c1d55ff104554fb83349cb3
Jorge Do Carmo <jorge.do.carmo123@gmail.com>
2018-11-01 11:35:59 -0300

    Expondo header Authorization

--- src/main/java/jdc/loja/security/JWTAuthenticationFilter.java
+++ src/main/java/jdc/loja/security/JWTAuthenticationFilter.java
@@ -56,6 +56,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		String username = ((UserSS) auth.getPrincipal()).getUsername();
         String token = jwtUtil.generateToken(username);
         resp.addHeader("Authorization", "Bearer " + token);
+        resp.addHeader("access-control-expose-headers", "Authorization");
 	}
 	
 	private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
