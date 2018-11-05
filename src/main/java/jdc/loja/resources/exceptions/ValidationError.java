6700f1691160e64f3112334e6ad2c5a1b240d3d9
Jorge Do Carmo <jorge.do.carmo123@gmail.com>
2018-11-05 12:29:40 -0200

    Padronizando formato de exceções

--- src/main/java/jdc/loja/resources/exceptions/ValidationError.java
+++ src/main/java/jdc/loja/resources/exceptions/ValidationError.java
@@ -15,9 +15,9 @@ public class ValidationError extends StandardError {
 	public void addError(String fieldName, String message) {
 		errors.add(new FieldMessage(fieldName, message));
 	}
-	
-	public ValidationError(Integer status, String msg, Long timeStamp) {
-		super(status, msg, timeStamp);
-	}
 
+	public ValidationError(Long timestamp, Integer status, String error, String message, String path) {
+		super(timestamp, status, error, message, path);
+	}
+	
 }
