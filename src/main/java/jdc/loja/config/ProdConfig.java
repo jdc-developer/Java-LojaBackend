55fe03db480b1a60ef6f63721c8ef6982efc8722
Jorge Do Carmo <jorge.do.carmo123@gmail.com>
2018-11-07 16:44:13 -0200

    Ajustes para Produção

---
+++ src/main/java/jdc/loja/config/ProdConfig.java
@@ -0,0 +1,38 @@
+package jdc.loja.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import jdc.loja.services.DBService;
+import jdc.loja.services.EmailService;
+import jdc.loja.services.SmtpEmailService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
+}
